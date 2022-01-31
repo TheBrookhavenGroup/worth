9d656811a3c70d8980c7ed9f4174b31f1db75875
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-01-31 16:25:07 -0500

    Starting git repo.

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=50, unique=True, blank=False)
+    owner = models.CharField(max_length=50)
+    broker = models.CharField(max_length=50)
+    broker_account = models.CharField(max_length=50, unique=True, blank=False)
+    description = models.CharField(max_length=200, blank=True)
