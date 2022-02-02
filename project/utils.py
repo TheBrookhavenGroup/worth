1c195c8df88e78b8debea06a6217a37160748f23
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-02-02 14:51:15 -0500

    Added Trades model.

---
+++ project/utils.py
@@ -0,0 +1,14 @@
+import datetime
+import pytz
+from django.conf import settings
+
+
+def our_now():
+    # This results in a time that can be compared values in our database
+    # even if saved by a human entering wall clock time into an admin field.
+    # This is in the time zone specified in settings, for us it is 'America/New_York'.
+    return datetime.datetime.now(tz=pytz.timezone(settings.TIME_ZONE))
+
+
+def yyyymmdd2dt(d):
+    return datetime.strptime(str(d), '%Y%m%d')
--- trades/models.py
+++ trades/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from markets.models import Ticker
+from accounts.models import Account
 
-# Create your models here.
+class Trade(models.Model):
+    dt = models.DateTimeField(null=False, blank=False)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    ticker = models.ForeignKey(Ticker, on_delete=models.CASCADE)
+    reinvest = models.BooleanField(default=True, blank=False, null=False)
+    q = models.FloatField(blank=False, null=False)
+    p = models.FloatField(blank=False, null=False)
+    commission = models.FloatField(blank=True, null=False)
+    note = models.CharField(max_length=180, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.dt} {self.ticker.ticker} {self.q} @ {self.p}"
+
+    def save(self, *args, **kwargs):
+        if self.commission is None:
+            self.commission = self.q * self.ticker.market.commission
+        super().save(*args, **kwargs)
+
+    def calc_commission(self):
+        return self.q * self.ticker.market.commission
+
