e952d14913f08bdc2474c637d3bd07adac711010
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-02-02 14:59:54 -0500

    Added Trades model.

--- trades/admin.py
+++ trades/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Trade
 
-# Register your models here.
+
+@admin.register(Trade)
+class MarketAdmin(admin.ModelAdmin):
+    list_display = ('dt', 'account', 'ticker', 'q', 'p')
+
