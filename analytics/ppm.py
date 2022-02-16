bc8c1afb14af52a302cdfea5c78f57d6f3d99dfb
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-02-15 20:36:07 -0500

    Cleaned up some code.

--- analytics/ppm.py
+++ analytics/ppm.py
@@ -1,26 +1,30 @@
 from django.db.models import Sum
 from markets.tbgyahoo import yahooQuote
 from collections import defaultdict
-from functools import cache
+from cachetools.func import ttl_cache
+
 from worth.utils import cround
 from trades.models import Trade
 from accounts.models import CashRecord
 from worth.utils import is_near_zero
 
 
-default_prices = defaultdict(lambda: 1.0)
-default_prices['V-NPI'] = 0.0
-default_prices['V-KOL'] = 0.0
-default_prices['V-MED'] = 0.0
-default_prices['V-CAR'] = 0.0
-default_prices['V-CAD'] = 0.0
-default_prices['V-FND'] = 0.0
-default_prices['V-TSA'] = 0.0
-for i in ['cash', 'SNAXX', 'FBroker', 'FDRXX']:
-    default_prices[i] = 1.0
+default_prices = dict([
+    ('V-NPI', 0.0),
+    ('V-KOL', 0.0),
+    ('V-MED', 0.0),
+    ('V-CAR', 0.0),
+    ('V-CAD', 0.0),
+    ('V-FND', 0.0),
+    ('V-TSA', 0.0),
+    ('cash', 1.0),
+    ('SNAXX', 1.0),
+    ('FBroker', 1.0),
+    ('FDRXX', 1.0)
+])
 
 
-@cache
+@ttl_cache(maxsize=128, ttl=60)
 def get_price(ticker):
     if ticker in default_prices:
         p = default_prices[ticker]
@@ -31,27 +35,17 @@ def get_price(ticker):
 
 def get_balances():
     # balances[<account>]->[<symbol>]-><qty>
-    balances = defaultdict(lambda: {})
+    balances = defaultdict(lambda: defaultdict(lambda: 0.0))
 
     qs = Trade.objects.values_list('account__name', 'ticker__ticker', 'reinvest', 'q', 'p', 'commission')
     for a, ticker, reinvest, q, p, c in qs:
-        if a in balances:
-            portfolio = balances[a]
-        else:
-            portfolio = {}
-            balances[a] = portfolio
+        portfolio = balances[a]
 
         if not reinvest:
             cash_amount = -q * p - c
-            if 'cash' not in portfolio:
-                portfolio['cash'] = cash_amount
-            else:
-                portfolio['cash'] += cash_amount
+            portfolio['cash'] += cash_amount
 
-        if ticker not in portfolio:
-            portfolio[ticker] = q
-        else:
-            portfolio[ticker] += q
+        portfolio[ticker] += q
 
     qs = CashRecord.objects.values('account__name').order_by('account__name').annotate(total=Sum('amt'))
     for result in qs:
@@ -59,13 +53,7 @@ def get_balances():
         if abs(total) < 0.001:
             continue
         a = result['account__name']
-        if a not in balances:
-            balances[a] = {}
-        portfolio = balances[a]
-        if 'cash' not in portfolio:
-            portfolio['cash'] = total
-        else:
-            portfolio['cash'] += total
+        balances[a]['cash'] += total
 
     empty_accounts = [a for a in balances if abs(balances[a]['cash']) < 0.001]
     for a in empty_accounts:
@@ -87,10 +75,8 @@ def valuations():
             q = portfolio[ticker]
             if is_near_zero(q):
                 continue
-            if 'cash' == ticker:
-                p = 1.0
-            else:
-                p = get_price(ticker)
+
+            p = get_price(ticker)
             value = q * p
             total_worth += value
 
