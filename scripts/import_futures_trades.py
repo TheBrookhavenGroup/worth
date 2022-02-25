83110b878d4f0a34d32be3f90dcc345a90e8d99c
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-02-25 17:20:06 -0500

    Imported all MSRK Futures trades since BSTP.

--- scripts/import_futures_trades.py
+++ scripts/import_futures_trades.py
@@ -1,4 +1,5 @@
-import re
+
+from cachetools.func import ttl_cache
 from django.db import transaction
 from worth.utils import dt2dt
 from accounts.models import Account, CashRecord
@@ -7,6 +8,18 @@ from trades.models import Trade
 
 
 account = Account.objects.get(name='FUTURES')
+
+
+@ttl_cache(maxsize=128, ttl=60)
+def get_market(s):
+    try:
+        m = Market.objects.get(symbol=s)
+    except Market.DoesNotExist:
+        print(f'Making market for {s}')
+        m = Market.objects.get_or_create(symbol=s, name=s,
+                                         ib_exchange='CME', yahoo_exchange='CME')
+        m = m[0]
+    return m
 
 
 @transaction.atomic
@@ -35,7 +48,7 @@ def add_trades():
         try:
             ticker = Ticker.objects.get(ticker=t)
         except Ticker.DoesNotExist as e:
-            market = Market.objects.get(symbol=t[:-5])
+            market = get_market(t[:-5])
             print(f"Creating ticker {t}.")
             ticker = Ticker(ticker=t, market=market)
             ticker.save()
