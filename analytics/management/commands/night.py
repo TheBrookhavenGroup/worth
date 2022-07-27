1969fe0ed1091b50aa63e9c2f9df08955164a422
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-07-27 15:09:54 -0400

    Changed ppm manager command to night and it gets trades now.

--- analytics/management/commands/night.py
+++ analytics/management/commands/night.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand
 from analytics.pnl import year_pnl
+from trades.ib_flex import get_trades
 
 
 class Command(BaseCommand):
     help = 'Run PPM and store results in database.'
 
     def handle(self, *args, **options):
+        x = get_trades()
+        for i in x[1]:
+            print(i)
         x = year_pnl()
         print(next(i for i in x[1] if i[0] == 'ALL')[4])
