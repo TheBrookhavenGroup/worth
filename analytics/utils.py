6f4764db7db581b9c5281ff10aa9418faf8f3828
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-12-30 13:18:03 -0500

    Added total to total_realized_gains().

--- analytics/utils.py
+++ analytics/utils.py
@@ -43,4 +43,8 @@ def total_realized_gains(year):
 
     realized = pd.concat([realized, df])
 
+    total = pd.DataFrame({'a': ['Total'], 't':[''], 'realized': [realized.realized.sum()]})
+
+    realized = pd.concat([realized, total])
+
     return realized, format_realized_rec
