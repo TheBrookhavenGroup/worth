4a40ad6bce81d92dd2bccc38ac44a0cff2ead1ee
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2023-06-02 16:53:09 -0400

    Use pd.concat rather than df.append().

--- trades/utils.py
+++ trades/utils.py
@@ -152,7 +152,7 @@ def pnl_asof(d=None, a=None, only_non_qualified=False, active_f=True):
     # cash columsn are 'a' and 'q'.
     # pnl columns are 'a', 't', 'e' and 'q' where e=='CASH'
     money_markets = pnl[pnl.e == 'CASH'][["a", "q"]]
-    cash = cash.append(money_markets)
+    cash = pd.concat([cash, money_markets], axis=0)
     cash = cash.groupby('a').sum().reset_index()
 
     return pnl, cash
