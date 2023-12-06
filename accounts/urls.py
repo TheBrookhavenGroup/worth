e275bf453ea3b1b5cb6bbc907257995605021f87
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2023-12-06 15:02:34 -0500

    Added the receivables view.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import GetIBStatementsView, AccountsView
+from .views import GetIBStatementsView, AccountsView, ReceivablesView
 
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('ib_statements/', GetIBStatementsView.as_view(), name='get_ib_statements'),
+    path('receivables/', ReceivablesView.as_view(), name='receivables'),
     path('accounts/', AccountsView.as_view(), name='accounts'),
 ]
