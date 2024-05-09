50e9483aa003616911ec58533589a60a7c9e3b0f
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2024-05-09 12:10:38 -0400

    Handle div by zero for wap.

--- worth/settings.py
+++ worth/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'crispy_forms',
+    'crispy_bootstrap4',
     'easyaudit',
     'accounts',
     'markets',
