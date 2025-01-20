30694b83413ebcb08553ee43da7a7a8e9556ad27
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2025-01-20 13:48:28 -0500

    Added realized.html and csv export link for taxable income. (#260)
    * Removed media from urls.py.
    
    * Made realized.html with csv export link.

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (PnLView, GetIBTradesView,
                     TickerView, ValueChartView, RealizedGainView,
-                    PnLIfClosedView, IncomeExpenseView, income_csv_view,
-                    expenses_csv_view)
+                    realized_csv_view,
+                    PnLIfClosedView, IncomeExpenseView,
+                    income_csv_view, expenses_csv_view)
 
 
 app_name = 'analytics'
@@ -13,6 +14,7 @@ urlpatterns = [
     path('ticker/<ticker>/', TickerView.as_view(), name='ticker_view'),
     path('value_chart/', ValueChartView.as_view(), name='value_chart'),
     path('realized/', RealizedGainView.as_view(), name='realized'),
+    path('realized/csv/<int:param>', realized_csv_view, name='realizedcsv'),
     path('pnlifclosed/', PnLIfClosedView.as_view(), name='pnlifclosed'),
     path('incomeexpense/', IncomeExpenseView.as_view(), name='incomeexpense'),
     path('income/csv/<int:param>', income_csv_view, name='incomecsv'),
