d6b3646f62e8b582ab5a8a32259280af5881bf79
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2025-09-17 08:41:13 -0400

    Moved static from worth dir to project dir.

--- worth/settings.py
+++ worth/settings.py
@@ -150,9 +150,7 @@ mc_dt.time_zone = TIME_ZONE
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'worth/static'),
-]
+#STATICFILES_DIRS = ['static/']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
