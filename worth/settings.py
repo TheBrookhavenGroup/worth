55c9eb6e9630630f4ad8b2d1b1ce4aef5b14b6cf
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2025-11-12 17:29:14 -0500

    Added default value for settings.FIFO.

--- worth/settings.py
+++ worth/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 mc_dt.time_zone = TIME_ZONE
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ('static',)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
