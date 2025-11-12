c7f62314e41b0a84b14bed727adf53cc24d320e5
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2025-11-12 13:17:12 -0500

    Added default value for settings.FIFO.

--- worth/settings.py
+++ worth/settings.py
@@ -39,6 +39,7 @@ else:
     GPG_PASS = None
     GPG_EMAIL = 'you@example.com'
     PPM_FACTOR = 1
+    FIFO = True`
 
 # Build paths inside the worth like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
